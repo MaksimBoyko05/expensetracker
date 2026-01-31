ea90d17b9b78bf91af4effa5969f68e516b50f92
MaksimBoyko05 <vladosmakasin1@gmail.com>
2026-01-31 20:31:15 +0200

    feat: Implement save and get data from localctorage

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -10,7 +10,7 @@ function AddExpenseForm({expenses, setExpenses, categories}) {
   }
   const [data, setData] = useState(initialState)
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault()
 
     const newExpense = {
@@ -20,32 +20,49 @@ function AddExpenseForm({expenses, setExpenses, categories}) {
         ? data.customCategory
         : data.category
     }
-    setExpenses([
-      ...expenses, newExpense
-    ])
-    setData(initialState)
+    if (data.name && data.price && newExpense.category) {
+      setExpenses([
+        ...expenses, newExpense
+      ])
+      setData(initialState)
+    } else {
+      alert("Будь ласка, заповніть всі поля!")
+    }
   }
+
   return (
-<>
-<div>
-  <form onSubmit={handleSubmit}>
-    <input type={"text"} value={data.name} onChange={(e) => setData({...data, name: e.target.value})}/>
-    <input type={"text"} value={data.price} onChange={(e) => setData({...data, price: e.target.value})}/>
-    <select value={data.category} onChange={(e) => setData({...data, category: e.target.value})}>
-      {categories.map(ctg =>  (
-        <option key={ctg}>{ctg}</option>
-      ))}
-      <option value={"custom"}>+ Створити нову</option>
-    </select>
-    {data.category === "custom" && (
-      <>
-        <input type={"text"} value={data.customCategory} onChange={(e) => setData({...data, customCategory: e.target.value})}/>
-      </>
-      )}
-    <button type="submit">Додати витрату</button>
-  </form>
-</div>
-</>
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type={"text"}
+            value={data.name}
+            onChange={(e) => setData({...data, name: e.target.value})}/>
+          <input
+            type={"text"}
+            value={data.price}
+            onChange={(e) => setData({...data, price: e.target.value})}/>
+          <select
+            value={data.category}
+            onChange={(e) => setData({...data, category: e.target.value})}>
+            {categories.map(ctg => (
+              <option key={ctg}>{ctg}</option>
+            ))}
+            <option value={"custom"}>+ Створити нову</option>
+          </select>
+          {data.category === "custom" && (
+            <>
+              <input
+                type={"text"}
+                value={data.customCategory}
+                onChange={(e) => setData({...data, customCategory: e.target.value})}/>
+            </>
+          )}
+          <button type="submit">Додати витрату</button>
+        </form>
+      </div>
+    </>
   );
 }
+
 export default AddExpenseForm;
--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -1,48 +1,65 @@
 import {useState, useEffect} from "react";
 import Form from "./AddExpenseForm";
 
-function ExpenseTracker (){
-  const initialExpenses = [
-    { id: 1, name: "Кава", price: 65, category: "Їжа", date: "2024-01-28" },
-    { id: 2, name: "Книга React", price: 450, category: "Навчання", date: "2024-01-25" },
-    { id: 3, name: "Кіно", price: 180, category: "Розваги", date: "2024-01-20" },
-    { id: 4, name: "Продукти", price: 1200, category: "Їжа", date: "2024-01-29" },
-    { id: 5, name: "Таксі", price: 150, category: "Транспорт", date: "2024-01-29" },
-  ];
-  const [expenses, setExpenses] = useState(initialExpenses)
-  const [selectedCategory, setSelectedCategory] = useState("Їжа")
+function ExpenseTracker() {
+  const [expenses, setExpenses] = useState(() => {
+    const savedExpenses = localStorage.getItem("expenses")
+    if (savedExpenses) {
+      return JSON.parse(savedExpenses)
+    } else {
+      return []
+    }
+  })
+  const [selectedCategory, setSelectedCategory] = useState("All")
 
   const sortExpenses = () => {
     let newExpenses = [...expenses]
-      setExpenses(newExpenses.sort((a, b) => a.price - b.price))
+    setExpenses(newExpenses.sort((a, b) => a.price - b.price))
   }
 
   const filteredExpenses = selectedCategory === "All"
-  ? expenses
-    : expenses.filter(item =>  item.category === selectedCategory)
+    ? expenses
+    : expenses.filter(item => item.category === selectedCategory)
 
-  const categories = ["All", ...new Set(expenses.map(exp =>(exp.category)))]
-  const totalExpense = filteredExpenses.reduce((accumulator, currentValue) =>{
-    return accumulator +  currentValue.price
+  const categories = ["All", ...new Set(expenses?.map(exp => (exp.category)))]
+  const totalExpense = filteredExpenses.reduce((accumulator, currentValue) => {
+    return accumulator + Number(currentValue.price)
   }, 0);
+  useEffect(() => {
+    try {
+      localStorage.setItem("expenses", JSON.stringify(expenses))
+    } catch (error) {
+      console.log("Error with get data", error)
+    }
+  }, [expenses]);
   return (
     <>
       <div>
-       <ul>
-         {filteredExpenses.map(exp =>(
-           <li key={exp.id}>{exp.name} - {exp.price} грн</li>
-         ))}
-       </ul>
+        {expenses.length === 0 ? (
+          <p>У вас ще немає витрат</p>
+        ) : (
+          <ul>
+            {filteredExpenses?.map(exp => (
+              <li key={exp.id}>{exp.name} - {exp.price} грн</li>
+            ))}
+          </ul>
+        )}
         <p>Усього: {totalExpense}</p>
         <button onClick={sortExpenses}>Сортувати</button>
-        <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
-          {categories.map(ctg =>  (
+        <select
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}>
+          {categories.map(ctg => (
             <option key={ctg}>{ctg}</option>
           ))}
         </select>
-        <Form expenses={expenses} setExpenses={setExpenses} categories={categories}/>
+        <Form
+          expenses={expenses}
+          setExpenses={setExpenses}
+          categories={categories}/>
       </div>
     </>
   );
 }
-export default  ExpenseTracker;
+
+export default ExpenseTracker;
